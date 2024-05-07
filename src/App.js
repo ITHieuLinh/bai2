9ecac735b2c0b03e563b65c1d6d534da5dd32745
ITHieuLinh <hoanghieulinh362@gmail.com>
2024-05-07 22:36:50 +0700

    second commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>FullName: Hoàng Hiếu Linh </p>
+        <p>Class: SE17D08</p>
+        <p>School: FPT UNIVERSITY</p>
+        <p>Description: I am 22 years old. My major is SE.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
